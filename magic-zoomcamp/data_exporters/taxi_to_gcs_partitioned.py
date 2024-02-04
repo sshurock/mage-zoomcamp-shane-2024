579cda680f50cdcb5a91ccf89817999aef23b745
Shane Shurock <shaneshurock@Shanes-MacBook-Pro.local>
2024-02-04 11:47:47 -0500

    initial commit

---
+++ magic-zoomcamp/data_exporters/taxi_to_gcs_partitioned.py
@@ -0,0 +1,41 @@
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.google_cloud_storage import GoogleCloudStorage
+from pandas import DataFrame
+from os import path
+import pyarrow as pa
+import pyarrow.parquet as pq
+import os
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/src/placeholder.json"
+
+bucket_name = 'mage-zoomcamp-shane'
+project_id = "de-zoomcamp-shane"
+
+table_name = "green_taxi_data"
+
+root_path = f'{bucket_name}/{table_name}'
+
+@data_exporter
+def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
+    """
+    Template for exporting data to a Google Cloud Storage bucket.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#googlecloudstorage
+    """
+    # data["lpep_pickup_date"] = data["lpep_pickup_date"].dt.date
+    
+    table = pa.Table.from_pandas(df)
+
+    gcs = pa.fs.GcsFileSystem()
+
+    pq.write_to_dataset(
+        table,
+        root_path=root_path,
+        partition_cols=['lpep_pickup_date'],
+        filesystem=gcs
+    )
---
+++ magic-zoomcamp/transformers/green_taxi_transformation.py
@@ -0,0 +1,49 @@
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+def to_snake_case(name):
+    import re
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+@transformer
+def transform(data, *args, **kwargs):
+    """
+    Template code for a transformer block.
+
+    Add more parameters to this function if this block has multiple parent blocks.
+    There should be one parameter for each output variable from each parent block.
+
+    Args:
+        data: The output from the upstream parent block
+        args: The output from any additional upstream blocks (if applicable)
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your transformation logic here
+    data = data[(data["passenger_count"] > 0) & (data["trip_distance"] > 0)]
+
+    data.columns = data.columns.map(to_snake_case)
+    
+    data['lpep_pickup_date'] = data['lpep_pickup_datetime'].dt.date
+    data['lpep_dropoff_date'] = data['lpep_dropoff_datetime'].dt.date
+
+    print("Unique vendor_id values:", data.vendor_id.unique())
+
+    print(data.shape)
+
+    return data
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
+    assert 0 not in set(output.passenger_count), "Passenger counter gt 0"
+    assert 0 not in set(output.trip_distance), "Trip distance gt 0"
+    assert 'vendor_id' in output.columns, "vendor_id was normalized"
